ebab8adf2bb8af0cac2c52f41d06eed35481d0c8
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-12-18 14:02:35 -0600

    add neony palette into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         'peach-200': 'rgba(247, 204, 146, 0.2)',
         'light-brown': '#af6548',
         'ruddy-brown': '#8c3019',
-        'dark-brown': '#37190f'
+        'dark-brown': '#37190f',
+        space: '#000117',
+        teal: '#13a8fe',
+        'neon-pink': '#ff00ff',
+        fuscia: '#cf0060',
+        dandelion: '#ffa300'
       }
     }
   }
