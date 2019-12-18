d771b9b89a81dd538f87a8a5c4b6b7fa1f796f67
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-12-17 23:07:49 -0600

    styles for Top / New view

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import Title from './Title'
 import PostInfo from './PostInfo'
 
-
 const Post = ({ post }) => {
   console.log('post inside Post', post)
   return (
@@ -17,7 +16,6 @@ const Post = ({ post }) => {
       <PostInfo
         by={post.by}
         descendants={post.descendants}
-        kids={post.kids}
         id={post.id}
         time={post.time}
         score={post.score}
--- src/components/PostInfo.js
+++ src/components/PostInfo.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom'
 
 import { formatDateTimeFrom } from '../utils/helpers'
 
-const PostInfo = ({ by, descendants = 0, kids = [], id, score = 0, time }) => {
-  const linkToCommentsLogic = descendants ? <Link to={`/post?id=${kids.slice(0,1)}`}>{descendants}</Link> : 0
+const styles = {
+  link: ['underline', 'font-black']
+}
 
+const PostInfo = ({ by, descendants = 0, id, score = 0, time }) => {
   return (
-    <div>
+    <div className='text-sm text-gray-800'>
       <span>{score} points </span>
-      <span>by <Link to={`/user?id=${by}`}>{by}</Link></span>
-      <span> {formatDateTimeFrom(time)} with <Link to={`/post?id=${id}`}>{descendants}</Link> comments</span>
+      <span>by <Link className={styles.link.join(' ')} to={`/user?id=${by}`}>{by}</Link></span>
+      <span> {formatDateTimeFrom(time)} with <Link className={styles.link.join(' ')} to={`/post?id=${id}`}>{descendants}</Link> comments</span>
     </div>
   )
 }
@@ -19,7 +21,6 @@ const PostInfo = ({ by, descendants = 0, kids = [], id, score = 0, time }) => {
 PostInfo.propTypes = {
   by: PropTypes.string.isRequired,
   descendants: PropTypes.number,
-  kids: PropTypes.array,
   id: PropTypes.number.isRequired,
   score: PropTypes.number,
   time: PropTypes.number.isRequired
